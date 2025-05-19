9303711e4f9dee27d4cd55bda7e4368bebc6d58a
krishnaher0 <krishna613460@gmail.com>
2025-05-19 16:02:37 +0545

    Dashboard screen of Users learning platforms

---
+++ lib/view/dashboard_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class DashboardScreen extends StatelessWidget {
+  const DashboardScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Image.asset("assets/images/dashboard1.png"),
+    );
+  }
+}
