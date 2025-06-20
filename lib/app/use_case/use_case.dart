a58da6402621c6c6340b652f8346edff72850a66
krishnaher0 <krishna613460@gmail.com>
2025-06-20 16:02:09 +0545

    use case added for params and non params

---
+++ lib/app/use_case/use_case.dart
@@ -0,0 +1,12 @@
+
+import 'package:dartz/dartz.dart';
+
+import 'package:veda_learn/core/error/failure.dart';
+ 
+abstract interface class UseCaseWithParams<SuccessType, Params> {
+  Future<Either<Failure, SuccessType>> call(Params params);
+}
+ 
+abstract interface class UseCaseWithoutParams<SuccessType> {
+  Future<Either<Failure, SuccessType>> call();
+}
