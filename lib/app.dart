be41e8dd353fc08fe00397c33d55e40020fefeea
krishnaher0 <krishna613460@gmail.com>
2025-06-20 16:29:07 +0545

    all files added to complete authenticationa nd authorization using hive database

--- lib/app.dart
+++ lib/app.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:veda_learn/app/service_locator/service_locator.dart';
+import 'package:veda_learn/features/auth/presentation/view_model/login_view_model/login_viewmodel.dart';
+import 'package:veda_learn/features/auth/presentation/view_model/register_view_model/register_viewmodel.dart';
 import 'package:veda_learn/theme/theme_data.dart';
-import 'package:veda_learn/view/age_screen.dart';
-import 'package:veda_learn/view/dashboard_screen.dart';
-import 'package:veda_learn/view/email_screen.dart';
-import 'package:veda_learn/view/login_screen.dart';
-import 'package:veda_learn/view/name_screen.dart';
-import 'package:veda_learn/view/password_screen.dart';
-import 'package:veda_learn/view/register_welcome_screen.dart';
-import 'package:veda_learn/view/splash_screen.dart';
+import 'package:veda_learn/features/auth/presentation/view/age_screen.dart';
+import 'package:veda_learn/features/auth/presentation/view/dashboard_screen.dart';
+import 'package:veda_learn/features/auth/presentation/view/email_screen.dart';
+import 'package:veda_learn/features/auth/presentation/view/login_screen.dart';
+import 'package:veda_learn/features/auth/presentation/view/name_screen.dart';
+import 'package:veda_learn/features/auth/presentation/view/password_screen.dart';
+import 'package:veda_learn/features/auth/presentation/view/register_welcome_screen.dart';
+import 'package:veda_learn/features/splash/presentation/view/splash_screen.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -19,13 +23,33 @@ class App extends StatelessWidget {
 
       initialRoute: '/',
       routes: {
-        '/': (context) => const DashboardScreen(),
-        '/login': (context) => const LoginScreen(),
+        '/': (context) => const SplashScreen(),
+        '/login':
+            (context) => BlocProvider.value(
+              value: serviceLocator<LoginViewModel>(),
+              child: LoginScreen(),
+            ),
         '/register': (context) => const RegisterWelcomeScreen(),
-        '/agePage': (context) => const AgeScreen(),
-        '/namePage': (context) => const NameScreen(),
-        '/emailPage': (context) => const EmailScreen(),
-        '/password': (context) => PasswordScreen(),
+        '/age-screen':
+            (context) => BlocProvider.value(
+              value: serviceLocator<RegisterViewmodel>(),
+              child: AgeScreen(),
+            ),
+        '/name-screen':
+            (context) => BlocProvider.value(
+              value: serviceLocator<RegisterViewmodel>(),
+              child: NameScreen(),
+            ),
+        '/email-screen':
+            (context) => BlocProvider.value(
+              value: serviceLocator<RegisterViewmodel>(),
+              child: EmailScreen(),
+            ),
+        '/password-screen':
+            (context) => BlocProvider.value(
+              value: serviceLocator<RegisterViewmodel>(),
+              child: PasswordScreen(),
+            ),
         '/dashboard': (context) => DashboardScreen(),
       },
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,16 @@
+
+
 import 'package:flutter/material.dart';
 import 'package:veda_learn/app.dart';
+import 'package:veda_learn/app/service_locator/service_locator.dart';
+import 'package:veda_learn/core/network/hive_service.dart';
 
-void main() {
-  runApp(const App());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await initDependencies();
+
+  await HiveService().init();
+  
+  runApp(App());
 }
