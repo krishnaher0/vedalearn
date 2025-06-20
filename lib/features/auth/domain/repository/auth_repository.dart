f3140b0f61e0e15d1b43fdfec0668de9f3455329
krishnaher0 <krishna613460@gmail.com>
2025-06-20 16:27:39 +0545

    Auth repository added to the git

---
+++ lib/features/auth/domain/repository/auth_repository.dart
@@ -0,0 +1,9 @@
+import 'package:dartz/dartz.dart';
+import 'package:veda_learn/core/error/failure.dart';
+
+import 'package:veda_learn/features/auth/domain/entity/user_entity.dart';
+ 
+abstract interface class IAuthRepository {
+  Future<Either<Failure, void>> createAccount(UserEntity user);
+  Future<Either<Failure, String>> loginToAccount(String email, String password);
+}
