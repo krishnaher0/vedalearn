059a6dc640fdb5b1d15bace018456195fa467a2b
krishnaher0 <krishna613460@gmail.com>
2025-06-20 15:52:55 +0545

    User Entity added for User database

---
+++ lib/features/auth/domain/entity/user_entity.dart
@@ -0,0 +1,24 @@
+import 'package:equatable/equatable.dart';
+
+
+class UserEntity extends Equatable {
+  final String? id;
+final String? name;
+final int?age;
+final String? email;
+final String? password;
+
+  const UserEntity({
+    required this.name, 
+  required this.age, 
+  required this.email, 
+  required this.password, 
+  this.id
+  });
+
+  @override
+  List<Object?> get props => [
+   name,age,email,
+    password,
+  ];
+}
