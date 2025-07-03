fc29f9fa98fc6cdecdb46d1d03ff84233808e1de
krishnaher0 <krishna613460@gmail.com>
2025-07-03 20:32:19 +0545

    Connect frontend to the api endpoints

---
+++ lib/app/constant/api/api_endpoint.dart
@@ -0,0 +1,19 @@
+class ApiEndpoints {
+  ApiEndpoints._();
+ 
+  static const connectionTimeout = Duration(seconds: 1000);
+ 
+  static const receiveTimeout = Duration(seconds: 1000);
+ 
+  static const String serverAddress = "http://localhost:3001";
+ 
+  static const String baseUrl = "$serverAddress/api";
+ 
+  static const String imageUrl = "$serverAddress/uploads";
+ 
+  //Auth
+ 
+  static const String register = "/auth/register";
+ 
+  static const String login = "/auth/login";
+}
