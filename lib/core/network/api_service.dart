4a901ab32e243cc443f03463771aca62816485ac
krishnaher0 <krishna613460@gmail.com>
2025-07-22 23:51:25 +0545

    updated api service

--- lib/core/network/api_service.dart
+++ lib/core/network/api_service.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 import 'package:veda_learn/app/constant/api/api_endpoint.dart';
 import 'package:veda_learn/core/network/dio_error_interceptor.dart';
+import 'package:veda_learn/core/network/hive_service.dart';
 
  
 class ApiService {
@@ -24,6 +25,18 @@ class ApiService {
           responseHeader: true,
         ),
       )
+      ..interceptors.add(
+  InterceptorsWrapper(
+    onRequest: (options, handler) async {
+      final hiveService = HiveService();
+      final token = await hiveService.getToken();
+      if (token != null) {
+        options.headers['Authorization'] = 'Bearer $token';
+      }
+      return handler.next(options);
+    },
+  ),
+)
       ..options.headers = {
         'Accept': 'application/json',
  
